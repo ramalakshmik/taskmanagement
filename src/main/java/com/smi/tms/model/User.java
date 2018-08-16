f4d39297081ada6c409f1051cf4e9b9b345ed5fe
sys-user <sys-user@Admin>
2018-08-16 19:27:00 +0530

    i have modifies user file

--- src/main/java/com/smi/tms/model/User.java
+++ src/main/java/com/smi/tms/model/User.java
@@ -1,5 +1,20 @@
 package com.smi.tms.model;
 
 public class User {
+	
+	private String userName;
+	private int password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
 
 }
