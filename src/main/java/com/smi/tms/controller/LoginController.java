710dac86078ab794f13b0fa9efe41174d9b02ae3
sys-user <sys-user@VGS-024>
2018-08-17 09:20:25 +0530

    Spring version changed to 4.0.1

--- src/main/java/com/smi/tms/controller/LoginController.java
+++ src/main/java/com/smi/tms/controller/LoginController.java
@@ -1,5 +1,15 @@
 package com.smi.tms.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class LoginController {
+	
+	@RequestMapping("/login")
+	public ModelAndView showForm() {
+		return new ModelAndView("login");
+	}
 
 }
