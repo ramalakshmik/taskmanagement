06c6071d1c0cd3292a610403a0f5d4fa2d34a60c
Senthilkumar <senthilkumar.m@mookambikainfo.com>
2018-08-20 18:34:40 +0530

    login new form

--- src/main/java/com/smi/tms/controller/LoginController.java
+++ src/main/java/com/smi/tms/controller/LoginController.java
@@ -5,7 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.smi.tms.model.User;
@@ -17,12 +19,12 @@ public class LoginController {
 	@Autowired
 	LoginService loginService;
 
-	@RequestMapping("/login")
+	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public ModelAndView showForm(HttpServletRequest request,
-			HttpServletResponse response) {
+			HttpServletResponse response,@ModelAttribute("user")User user) {
 		String userName = "srilekha.vasanth@gmail.com"; //request.getParameter("userName");
 		String password = "analyst@123"; //request.getParameter("password");
-		User user = loginService.getUser(userName, password);
+		user = loginService.getUser(user.getUserName(), user.getPassword());
 		request.getSession().setAttribute("user", user);
 		// if (user.getRole().get(0).getRole().equalsIgnoreCase("admin")) {
 		return new ModelAndView("redirect:employeelist");
