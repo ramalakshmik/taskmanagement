a90a027e67809c3887b9a198ea79985ab1e563ac
Senthilkumar <senthilkumar.m@mookambikainfo.com>
2018-08-20 17:00:51 +0530

    tiles implementation

--- src/main/java/com/smi/tms/controller/LoginController.java
+++ src/main/java/com/smi/tms/controller/LoginController.java
@@ -1,24 +1,16 @@
 package com.smi.tms.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.smi.tms.model.User;
-import com.smi.tms.service.LoginService;
-import com.smi.tms.util.HibernateUtil;
 
 @Controller
 public class LoginController {
 	
-	@Autowired
-	LoginService loginService;
-	
-	@RequestMapping("/login")
+	@RequestMapping(value="/employeelist",method=RequestMethod.POST)
 	public ModelAndView showForm() {
-		HibernateUtil.hibernateTemplate.save(new User());
-		return new ModelAndView("employeelist");
+		return new ModelAndView("adminView");
 	}
 
 }
